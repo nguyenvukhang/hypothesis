ba6b19022b10a9d8d4f46c1dfbb47257d88110fc
nguyenvukhang <brew4k@gmail.com>
2024-08-06 23:39:53 +0800

    Somehow this works

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,43 @@ pub struct Mark<'a> {
 }
 
 pub trait Parse {
-    fn whack(&self) -> Option<Mark>;
+    fn whack<'a>(&'a self) -> Option<Mark<'a>>;
+    fn identity(&self) -> Self
+    where
+        Self: Sized;
 }
 
 impl Parse for &str {
-    fn whack(&self) -> Option<Mark> {
+    fn whack<'a>(&'a self) -> Option<Mark<'a>> {
         Some(Mark { line: &self[0..4] })
+    }
+    fn identity(&self) -> Self {
+        &self[0..1]
     }
 }
 
 pub trait ParseLines<'a> {
-    fn parse(&mut self) -> Vec<Mark>;
+    fn parse(&mut self) -> Vec<Mark<'a>>;
+    fn parse_str(&mut self) -> Vec<&str>;
 }
 
 impl<'a> ParseLines<'a> for Lines<'a> {
-    fn parse(&mut self) -> Vec<Mark> {
+    fn parse(&mut self) -> Vec<Mark<'a>> {
         let mut marks = vec![];
-        for (lnum, line) in self.enumerate() {
+        for line in self {
             if let Some(m) = line.whack() {
                 marks.push(m);
             }
         }
         marks
+    }
+
+    fn parse_str(&mut self) -> Vec<&str> {
+        let mut lines = vec![];
+        for line in self {
+            lines.push(line.identity());
+        }
+        lines
     }
 }
 
