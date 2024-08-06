6e5540552e06fc98dd4c38a000a00bdecd16531f
nguyenvukhang <brew4k@gmail.com>
2024-08-06 23:59:56 +0800

    wot

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,19 @@
-use std::str::Lines;
-
 pub struct Mark<'a> {
     line: &'a str,
 }
 
 pub trait Parse {
-    fn whack<'a>(&'a self) -> Option<Mark<'a>>;
-    fn identity(&self) -> Self
-    where
-        Self: Sized;
+    fn _parse<'a>(&'a self) -> Mark<'a>;
 }
 
 impl Parse for &str {
-    fn whack<'a>(&'a self) -> Option<Mark<'a>> {
-        Some(Mark { line: &self[0..4] })
-    }
-    fn identity(&self) -> Self {
-        &self[0..1]
+    fn _parse<'a>(&'a self) -> Mark<'a> {
+        Mark { line: &self }
     }
 }
 
-pub trait ParseLines<'a> {
-    fn parse(&mut self) -> Vec<Mark<'a>>;
-    fn parse_str(&mut self) -> Vec<&str>;
+fn test<'b>(x: &'b str) {
+    let m: Mark<'b> = x._parse();
 }
 
-impl<'a> ParseLines<'a> for Lines<'a> {
-    fn parse<'b>(&'b mut self) -> Vec<Mark<'a>> {
-        let mut marks: Vec<Mark<'a>> = vec![];
-        let mut lines: Vec<&'a str> = vec![];
-        for line in self {
-            let l: &'a str = line;
-            // lines.push(l);
-            let x: Mark<'a> = l.whack().unwrap();
-            // marks.push(x);
-        }
-        vec![]
-    }
-
-    fn parse_str(&mut self) -> Vec<&str> {
-        let mut lines = vec![];
-        for line in self {
-            lines.push(line.identity());
-        }
-        lines
-    }
-}
-
-fn parse() {}
-
-fn main() {
-    println!("Hello, world!");
-}
+fn main() {}
